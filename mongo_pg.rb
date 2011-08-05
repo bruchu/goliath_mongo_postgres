bf5611a40ec46d5c64d06088a7bafddd9154eed9
Bruce Chu <bruchu@yahoo.com>
2011-08-04 18:53:36 -0700

    not waiting on mongo response

--- mongo_pg.rb
+++ mongo_pg.rb
@@ -29,10 +29,40 @@ require 'active_record'
 class Partner < ActiveRecord::Base
 end
 
+# class AuthAndRateLimitReceiver < Goliath::Synchrony::MultiReceiver
+
+#   def initialize(env)
+#     @env = env
+#     @pending_queries = 0
+#     @db = env.config[db_name]
+#   end
+  
+#   def pre_process
+
+#     validate_app_key!
+
+#     @env.mongo.find( { :_id => self.usage_id }, :limit => 1 ).limit(1).each do |doc|
+#       self.usage_info = doc if doc
+#     end
+
+#     check_rate_limit!
+#     check_signature!
+#   end
+
+#   def post_process
+
+#   end
+# end
+
 class MongoPg < Goliath::API
   include Goliath::Validation # errors
 
   use Goliath::Rack::Params
+
+  #use Goliath::Rack::AsyncAroundware, AuthAndRateLimitReceiver, env
+
+  TIMEBIN_SIZE = 60 * 60
+  DEFAULT_RATE_LIMIT = 10
 
   class MissingApikeyError     < BadRequestError   ; end
   class RateLimitExceededError < ForbiddenError    ; end
@@ -46,25 +76,26 @@ class MongoPg < Goliath::API
   end
 
   def response(env)
-    start_time = Time.now.to_f
-
-    params = {:head => env['client-headers'], :query => env.params}
-
-    env.mongo.find( { :_id => 'test-bucket'}, :limit => 1 ).limit(1).each do |doc|
-      self.usage_info = doc if doc
-    end
-
-    self.partner = Partner.find_by_key('a')
-
-    unless self.partner
-      raise InvalidApikeyError 
-    end
-
-    puts self.usage_info.inspect
-    puts self.partner.inspect
-
-    # XXX: check signature
+    self.partner = nil
+    self.usage_info = nil
     
+    start_time = Time.now.to_f
+    params = {:head => env['client-headers'], :query => env.params}
+    validate_app_key!
+
+    #self.usage_info = env.mongo.find_one( { :_id => self.usage_id } )
+    rr = env.mongo.first( { :_id => self.usage_id } )
+    rr.callback do |docs|
+      puts 'docs.inspect=%s' % docs if docs
+    end
+
+    rr.errback do |err|
+      raise *err
+    end
+    
+    check_rate_limit!
+    check_signature!
+
     req = EM::HttpRequest.new("#{env.forwarder}#{env[Goliath::Request::REQUEST_PATH]}")
     resp = case(env[Goliath::Request::REQUEST_METHOD])
            when 'GET'  then req.get(params)
@@ -82,9 +113,7 @@ class MongoPg < Goliath::API
 
     #record(env, process_time, resp, env['client-headers'], response_headers)
 
-    EM.next_tick do
-      env.mongo.safe_update({ :_id => "test-bucket"}, { '$inc' => { :calls => 1 } }, :upsert => true)
-    end
+    charge_usage
 
     [resp.response_header.status, response_headers, resp.response]
   end
@@ -123,4 +152,70 @@ class MongoPg < Goliath::API
       e.mongo.insert(doc)
     end
   end
+
+
+  def validate_app_key!
+    if env.params['app'].to_s.empty?
+      raise MissingApikeyError
+    end
+
+    self.partner = Partner.find_by_key(env.params['app'])
+    puts self.partner.inspect
+    raise MissingApikeyError unless self.partner
+  end
+
+  def check_signature!
+    unless self.partner.secret
+      raise InvalidApikeyError
+    end
+  end
+
+  def check_rate_limit!
+    puts 'usage_info=%s' % self.usage_info.inspect
+
+    # check for UsageInfo document not found case
+    return unless usage_info
+    
+    if usage_info['calls'].to_f > ( partner['max_call_rate'] || DEFAULT_RATE_LIMIT ).to_f
+      charge_overlimit
+      raise RateLimitExceededError
+    end
+  end
+
+  def charge_usage
+    e = env
+    EM.next_tick do
+      e.mongo.safe_update({ :_id => usage_id }, { '$inc' => { :calls => 1 } }, :upsert => true)
+      e.mongo.find( { :_id => self.usage_id }, :limit => 1 ).limit(1).each do |doc|
+        puts doc.inspect if doc
+      end
+    end
+  end
+
+  def charge_overlimit
+    e = env
+    EM.next_tick do
+      e.mongo.safe_update({ :_id => usage_id }, { '$inc' => { :overlimit => 1 } }, :upsert => true)
+    end
+  end
+
+  # ===========================================================================
+
+  def usage_id
+    puts "#{ self.partner.id }-#{timebin}"
+    "#{ self.partner.id }-#{timebin}"
+  end
+
+  def timebin
+    @timebin ||= timebin_beg
+  end
+
+  def timebin_beg
+    ((Time.now.to_i / TIMEBIN_SIZE).floor * TIMEBIN_SIZE)
+  end
+
+  def timebin_end
+    timebin_beg + TIMEBIN_SIZE
+  end
 end
+
